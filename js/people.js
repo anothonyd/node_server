246e34bf5ae72c7666f55ce615a597a49354faac
anothonyd <anthony.dulguime.dev@gmail.com>
2023-08-03 18:12:51 +0800

    initial commit

---
+++ js/people.js
@@ -0,0 +1,12 @@
+const people = [
+    "yoshi",
+    "ryu",
+    "chuck",
+    "mario"
+]
+
+const ages = [20, 30, 40, 50, 60];
+
+module.exports = {
+    people, ages
+};
---
+++ js/test.js
@@ -0,0 +1,17 @@
+// const firstName = "yoshi";
+// console .log(firstName);
+
+// console.log(global);
+
+// global.setTimeout(() => {
+//     console.log("in the timeout")
+//     clearInterval(int);
+// }, 5000);
+
+// const int = setInterval(() => {
+//     console.log('hotdog');
+// }, 1500);
+
+
+console.log(__dirname);
+console.log(__filename);
